7c063372dd82d4c5b1d89e1bd5d0d300306c1f29
HaniyeDoustkani <s2510892041@fhooe.at>
2026-04-30 12:30:34 +0200

    Update model.time.js

--- js/model.time.js
+++ js/model.time.js
@@ -1,1 +1,15 @@
 "use strict";
+
+export function getTime() {
+    const time = new Date();
+
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    return {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+}
