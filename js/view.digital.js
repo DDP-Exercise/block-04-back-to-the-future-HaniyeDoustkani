6963aaa2d0b9150b429048aed5a12edbc24c47b0
HaniyeDoustkani <s2510892041@fhooe.at>
2026-04-30 12:30:29 +0200

    Update view.digital.js

--- js/view.digital.js
+++ js/view.digital.js
@@ -1,1 +1,13 @@
 "use strict";
+
+export function renderDigital(time) {
+    const clock = document.getElementById("clock");
+
+    const hours = String(time.hours).padStart(2, "0");
+    const minutes = String(time.minutes).padStart(2, "0");
+    const seconds = String(time.seconds).padStart(2, "0");
+
+    const text = hours + ":" + minutes + ":" + seconds;
+
+    clock.textContent = text;
+}
